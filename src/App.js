b588ecf11c05adfda676836946c36d2562c34bc6
Ben Hopkins <bjhops17@gmail.com>
2022-02-23 22:36:30 -0800

    test

--- src/App.js
+++ src/App.js
@@ -8,34 +8,35 @@ import MyResume from './components/pages/Resume';
 import Footer from "./components/Footer/Footer";
 //import 'bootstrap/dist/css/boostrap.min.css';
 import * as ReactBootStrap from "react-bootstrap";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="page-container">
     <div className="content-wrap">
+    <Router>
     <ReactBootStrap.Navbar bg="light" expand="lg">
   <ReactBootStrap.Container>
     <ReactBootStrap.Navbar.Brand href="#home">My React Portfolio by JC</ReactBootStrap.Navbar.Brand>
     <ReactBootStrap.Navbar.Toggle aria-controls="basic-navbar-nav" />
     <ReactBootStrap.Navbar.Collapse id="basic-navbar-nav">
       <ReactBootStrap.Nav className="me-auto">
-        <ReactBootStrap.Nav.Link href="/JC-React-Portfolio/home">Home</ReactBootStrap.Nav.Link>
-        <ReactBootStrap.Nav.Link href="/JC-React-Portfolio/about">About Me</ReactBootStrap.Nav.Link>
-        <ReactBootStrap.Nav.Link href="/JC-React-Portfolio/resume">My Resume</ReactBootStrap.Nav.Link>
-        <ReactBootStrap.Nav.Link href="/JC-React-Portfolio/portfolio">My Portfolio</ReactBootStrap.Nav.Link>
-        <ReactBootStrap.Nav.Link href="/JC-React-Portfolio/contacts">My Contacts</ReactBootStrap.Nav.Link>
+        <Link href="/home">Home</Link>
+        <Link href="/about">About Me</Link>
+        <Link href="/resume">My Resume</Link>
+        <Link href="/portfolio">My Portfolio</Link>
+        <Link href="/contacts">My Contacts</Link>
       </ReactBootStrap.Nav>
     </ReactBootStrap.Navbar.Collapse>
   </ReactBootStrap.Container>
 </ReactBootStrap.Navbar>
-        <Router>
+       
           <div className="container">  
           <Routes>
-          <Route path="/JC-React-Portfolio/about" element={<AboutMe/>}/>
-          <Route path="/JC-React-Portfolio/resume" element={<MyResume/>}/>
-          <Route path="/JC-React-Portfolio/portfolio" element={<MyPortfolio/>}/>
-          <Route path="/JC-React-Portfolio/contacts" element={<MyContacts/>}/>
+          <Route path="/about" element={<AboutMe/>}/>
+          <Route path="/resume" element={<MyResume/>}/>
+          <Route path="/portfolio" element={<MyPortfolio/>}/>
+          <Route path="/contacts" element={<MyContacts/>}/>
           
           </Routes>
           </div>  
